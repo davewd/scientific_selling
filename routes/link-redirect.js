fa952070ecffc6d73db8df87e05891e6757b1123
davewd <davewd@me.com>
2019-06-30 22:09:33 +1000

    Updating link tracking and configs

--- routes/link-redirect.js
+++ routes/link-redirect.js
@@ -4,15 +4,8 @@ var router = express.Router();
 var path = require('path');
 
 /* GET users listing. */
-router.get('/:name/:delay', function(req, res, next) {
-    console.log(req.params.name);
-    console.log(req.params.delay);
-    
-    if (req.params['delay'])
-        var start = Date.now()
-        while (Date.now() < start + Math.min( req.params.delay * 1000, 30000)) {}
-
-    res.sendFile(path.resolve(__dirname, '../public/images/p.gif'));
+router.get('/:jwt', function(req, res, next) {
+    console.log(req.params.jwt);
 });
 
 module.exports = router;
