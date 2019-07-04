2b64e5333879a7ac3cb92109d49df6b4aa9b1d20
davewd <davewd@me.com>
2019-07-04 22:25:38 +1000

    Add Mongoose to confiure writing output

--- app.js
+++ app.js
@@ -4,18 +4,31 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var compression = require('compression');
-
-
-
-// import the actual aapi endpoints
-var pixelRouter = require('./routes/pixel');
-var linkRedirectRouter = require('./routes/link-redirect');
+var mongoose = require('mongoose');
 
 // Start the engine
 const app = express()
 var environment = app.get('env')
 var config = require('config-yml').load(environment)
 console.log('Environment: '+ environment);
+console.log('Mongo Environment: '+ config.mongo.url);
+
+
+//Set up default mongoose connection
+//Get the default connection
+//Bind connection to error event (to get notification of connection errors)
+var mongoDB = config.mongo.url;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// import the actual api endpoints
+var pixelRouter = require('./routes/pixel');
+var linkRedirectRouter = require('./routes/link-redirect');
+
+
+
+
 
 const port = 3000
 
@@ -30,6 +43,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use('/p', pixelRouter);
 app.use('/t', linkRedirectRouter);
+
+app.use(function (err, req, res, next) {
+    // handle error
+    console.log(err);
+  })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
--- routes/pixel.js
+++ routes/pixel.js
@@ -2,26 +2,59 @@ var express = require('express');
 var math = require('Math');
 var path = require('path');
 
+//Require Mongoose
+var mongoose = require('mongoose');
+
+//Define a schema
+var Schema = mongoose.Schema;
+
+var PixelModelSchema = new Schema({
+    pixelID: String,
+    timestamp: Date,
+    params: {},
+    request_header: {},
+    complete: Boolean
+});
+
+// Compile model from schema
+var PixelModel = mongoose.model('PixelModel', PixelModelSchema);
+
 var router = express.Router();
 /* GET users listing. */
 
-function write() { // Async function statment
-    new Promise((resolve, reject) => setTimeout(resolve, 3000)).then(function(){
-    console.log("ASynch Complete")});
-  }
+function write(name, request_header, params, complete) { 
+    // Async function statment
+    // prefer to queue and then write async via a batch ... but no time.
+    model_dict = {
+        pixelID: name,
+        timestamp: Date.now(),
+        params: params,
+        request_header: request_header,
+        complete: complete
+    }
+    console.log(model_dict);
+    new Promise((resolve, reject) => PixelModel.create(model_dict, function (err, model_instance) {
+        if (err) 
+            console.log("An Error has occurred : " + err );
+        else
+            console.log("ASynch Write Complete");
+    }));
+}
 
-router.get('/:name/:delay?', async function(req, res, next) {
+router.get('/:name/:delay?', async function (req, res, next) {
     console.log(req.params.name);
     console.log(req.params.delay);
-    console.log(JSON.stringify(req.headers));
-    write();
-    if (typeof req.params.delay !== 'undefined' || req.params.delay !== null)
-    new Promise((resolve, reject) => setTimeout(resolve, 
-        Math.min( req.params.delay * 1000, 30000))).then(function(){
-            res.sendFile(path.resolve(__dirname, '../public/images/p.gif'));
-    }).then(function(){console.log("Add Header info to Queue")});
 
-    
+    write(req.params.name, req.header, req.params, false);
+    if (typeof req.params.delay !== 'undefined' || req.params.delay !== null)
+        new Promise((resolve, reject) => setTimeout(resolve,
+            Math.min(req.params.delay * 1000, 30000))).then(function () {
+            res.sendFile(path.resolve(__dirname, '../public/images/p.gif'));
+        }).then(function () {
+            console.log("Add Header info to Queue")
+        }).catch(next);
+    write(req.params.name, req.header, req.params, true); // love to optimize the update
+
 });
 
 module.exports = router;
